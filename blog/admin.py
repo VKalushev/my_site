d3178e0eb1fb08767d31a79f7168aee67e4742ba
VKalushev <vasko.kalushev@gmail.com>
2024-07-20 12:28:16 +0300

    adding db and models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Post,Author,Tag
+
+class PostAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    prepopulated_fields = {'slug': ("title",)}
+    list_filter = ("date","title","author")
+    list_display = ("title", "author","date")
+
+class AuthorAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    list_filter = ("first_name","last_name")
+    list_display = ("first_name", "last_name")
+
+class TagAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    list_filter = ("caption",)
+    list_display = ("caption",)
 
 # Register your models here.
+admin.site.register(Post,PostAdmin)
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(Tag,TagAdmin)
